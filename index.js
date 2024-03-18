01732f15bc2426f5ab126bc5b13d6e7494750352
Ichiden <denverbigayan1@gmail.com>
2024-03-18 17:20:40 +0800

    asdasdasdasdasd

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({
-    origin:"https://stellar-api-ca59.onrender.com",
+    origin:"https://stellar-gsfc.onrender.com",
     credentials:true
 }));
 
